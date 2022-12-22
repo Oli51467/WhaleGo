4401fc083e6b73123333305b96a661b2932efd41
Oli51467 <1832297226@qq.com>
2022-12-22 22:45:05 +0800

    feat:控制尾巴移动

--- src/assets/scripts/Snake.js
+++ src/assets/scripts/Snake.js
@@ -20,6 +20,7 @@ export class Snake extends GameObject {
         this.dc = [0, 1, 0, -1];    // 列偏移量
 
         this.step = 0;  // 当前回合数
+        this.eps = 1e-2;    // 距离误差
     }
 
     start() {
@@ -31,6 +32,12 @@ export class Snake extends GameObject {
         this.direction = d;
     }
 
+    check_tail_increasing() {
+        if (this.step <= 10) return true;
+        if (this.step % 3 === 1) return true;
+        return false;
+    }
+
     next_step() {   // 将蛇的状态变为走下一步 更新this.next_cell
         const d = this.direction;
         this.next_cell = new Cell(this.cells[0].r + this.dr[d], this.cells[0].c + this.dc[d]);
@@ -38,11 +45,39 @@ export class Snake extends GameObject {
         this.status = "move";
         this.step ++;
 
-        
+        const k = this.cells.length;
+        for (let i = k; i > 0; i -- ) {
+            this.cells[i] = JSON.parse(JSON.stringify(this.cells[i - 1]));  // 将旧的复制一份
+        }
     }
 
     update_move() { // 每秒钟控制蛇的移动
-        this.cells[0].x += this.speed * this.timedelta / 1000;
+        const dx = this.next_cell.x - this.cells[0].x;
+        const dy = this.next_cell.y - this.cells[0].y;
+        const distance = Math.sqrt(dx * dx, dy * dy);
+
+        if (distance < this.eps) {  // 接近于走完了一格的长度
+            this.cells[0] = this.next_cell; // 添加一个新蛇头
+            this.next_cell = null;
+            this.status = "idle";   // 走完了 更新状态
+
+            if (!this.check_tail_increasing()) {    // 蛇不变长就将蛇尾砍掉
+                this.cells.pop();
+            }
+        } else {
+            const move_distance = this.speed * this.timedelta / 1000;
+            this.cells[0].x += move_distance * dx / distance;   // x实际走的距离
+            this.cells[0].y += move_distance * dy / distance;
+
+            if (!this.check_tail_increasing()) {
+                const k = this.cells.length;
+                const tail = this.cells[k - 1], tail_target = this.cells[k - 2];
+                const tail_dx = tail_target.x - tail.x;
+                const tail_dy = tail_target.y - tail.y;
+                tail.x += move_distance * tail_dx / distance;
+                tail.y += move_distance * tail_dy / distance;
+            }
+        }
     }
 
     // 每一帧执行一次 每秒执行60次
