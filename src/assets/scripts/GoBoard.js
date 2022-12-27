9e6d84779e86e5ad69510c76f38df517a6edd87a
Oli51467 <1832297226@qq.com>
2022-12-27 21:43:09 +0800

    feat:落子

--- src/assets/scripts/GoBoard.js
+++ src/assets/scripts/GoBoard.js
@@ -10,6 +10,9 @@ export class GoBoard extends GameObject {
         this.L = 0;
         this.cell_len = 0;
         this.stones = [];
+        this.is_black = true;
+        this.virtual_x = -1;
+        this.virtual_y = -1;
     }
 
     init_stones() {
@@ -29,8 +32,69 @@ export class GoBoard extends GameObject {
         }
     }
 
+    add_mouse_events() {
+        this.ctx.canvas.addEventListener("mousemove", e => {
+            let click_x = e.offsetX;
+            let click_y = e.offsetY;
+            let x = Math.floor(click_y / 32);
+            let y = Math.floor(click_x / 32);
+            if (x <= 0 || x > 19 || y <= 0 || y > 19 || this.stones[x][y]) {
+                this.virtual_x = -1;
+                this.virtual_y = -1;
+                return;
+            }
+            this.virtual_x = y;
+            this.virtual_y = x;
+        });
+        this.ctx.canvas.addEventListener("click", e => {
+            let click_x = e.offsetX;
+            let click_y = e.offsetY;
+            let x = Math.floor(click_y / 32);
+            let y = Math.floor(click_x / 32);
+            this.virtual_x = -1;
+            this.virtual_y = -1;
+            if (x <= 0 || x > 19 || y <= 0 || y > 19 || this.stones[x][y]) return;
+            // 调试 在前端落子
+            if (this.is_black) {
+                this.stones[x][y] = 1;
+            } else {
+                this.stones[x][y] = 2;
+            }
+            this.is_black = !this.is_black;
+            // if (d >= 0) {
+            //     this.store.state.game.socket.send(JSON.stringify({
+            //         event: "move",
+            //         direction: d,
+            //     }));
+            // }
+
+        });
+    }
+
     start() {
         this.init_stones();
+        this.add_mouse_events();
+    }
+
+    draw_virtual_stone(x, y) {
+        const center_x = x * this.cell_len;
+        const center_y = y * this.cell_len;
+        const r = this.cell_len / 2 * 0.9;
+        if (this.is_black) {
+            console.log(x, y);
+            let gradient = this.ctx.createRadialGradient(center_x, center_y, r / 1.8, center_x, center_y, 0);
+            gradient.addColorStop(0, "#333333");
+            gradient.addColorStop(1, "#4d4d4d");
+            this.ctx.fillStyle = gradient;
+        } else {
+            let gradient = this.ctx.createRadialGradient(center_x, center_y, r / 1.8, center_x, center_y, 0);
+            gradient.addColorStop(0, "#e6e6e6");
+            gradient.addColorStop(1, "#ffffff");
+            this.ctx.fillStyle = gradient;
+        } 
+        this.ctx.beginPath();
+        this.ctx.arc(center_x, center_y, r, 0, Math.PI * 2);
+        this.ctx.fill();
     }
 
     draw_lines() {
@@ -48,7 +112,7 @@ export class GoBoard extends GameObject {
     }
 
     draw_stars(x, y) {
-        this.ctx.fillStyle = "#402f26";
+        this.ctx.fillStyle = "#f402f26";
         this.ctx.beginPath();
         this.ctx.arc(x * this.cell_len, y * this.cell_len, this.cell_len / 5 * 0.75, 0, Math.PI * 2);
         this.ctx.fill();
@@ -64,14 +128,23 @@ export class GoBoard extends GameObject {
     }
 
     draw_stones(x, y, color) {
+        const center_x = x * this.cell_len;
+        const center_y = y * this.cell_len;
+        const r = this.cell_len / 2 * 0.9;
         if (color === 0) return;
         else if (color === 1) {
-            this.ctx.fillStyle = "black";
+            let gradient = this.ctx.createRadialGradient(center_x, center_y, r / 1.8, center_x, center_y, 0);
+            gradient.addColorStop(0, "#0a0a0a");
+            gradient.addColorStop(1, "#4d4d4d");
+            this.ctx.fillStyle = gradient;
         } else if (color === 2) {
-            this.ctx.fillStyle = "white";
+            let gradient = this.ctx.createRadialGradient(center_x, center_y, r / 1.8, center_x, center_y, 0);
+            gradient.addColorStop(0, "#e6e6e6");
+            gradient.addColorStop(1, "#ffffff");
+            this.ctx.fillStyle = gradient;
         } 
         this.ctx.beginPath();
-        this.ctx.arc(x * this.cell_len, y * this.cell_len, this.cell_len / 2 * 0.75, 0, Math.PI * 2);
+        this.ctx.arc(center_x, center_y, r, 0, Math.PI * 2);
         this.ctx.fill();
     }
 
@@ -83,9 +156,11 @@ export class GoBoard extends GameObject {
     render() {
         this.draw_lines();
         this.init_stars();
+        this.draw_stones(5, 5, 2);
         for (let r = 1; r <= this.rows; r ++ ) {
             for (let c = 1; c <= this.cols; c ++ ) {
                 this.draw_stones(c, r, this.stones[r][c]);
+                if (this.virtual_x != -1 && this.virtual_y != -1) this.draw_virtual_stone(this.virtual_x, this.virtual_y);
             }
         }
     }
--- src/components/GoPlayGround.vue
+++ src/components/GoPlayGround.vue
@@ -55,14 +55,12 @@ export default {
 }
 
 canvas {
-    background-color: #735444;
+    background-color: #6a4d3f;
     border: 1px solid #522;
     margin-top: 20px;
     display: inline-block;
     border: 1px solid #522;
-    background-image: -moz-radial-gradient(rgba(255, 255, 255, 0.3), rgba(0, 0, 0, 0));
-    background-image: -webkit-radial-gradient(rgba(255, 255, 255, 0.3), rgba(0, 0, 0, 0));
-    background-image: radial-gradient(rgba(255, 255, 255, 0.3), rgba(0, 0, 0, 0));
+    background-image: radial-gradient(rgba(255, 255, 255, 0.4), rgba(26, 25, 25, 0));
     -moz-box-shadow: 0 0 2em rgba(0, 0, 0, 0.5);
     -webkit-box-shadow: 0 0 2em rgba(0, 0, 0, 0.5);
     box-shadow: 0 0 2em rgba(0, 0, 0, 0.5);
