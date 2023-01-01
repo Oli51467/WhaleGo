7c5c621166f8afc10188e21ef31d365576b35c30
Oli51467 <1832297226@qq.com>
2023-01-01 15:10:52 +0800

    refactor:修改回调后端数据的格式

--- src/components/go/GoPlayBoard.vue
+++ src/components/go/GoPlayBoard.vue
@@ -65,7 +65,9 @@ export default {
 
         const click_resign = () => {
             store.state.gogame.socket.send(JSON.stringify({
-                event: "resign",
+                event: "play",
+                x: -1,
+                y: -1,
             }));
         }
 
