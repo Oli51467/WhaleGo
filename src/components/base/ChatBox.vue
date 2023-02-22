e5d2bc20939a9e1ddfd5616ce35e9f09f8e78944
Oli51467 <1832297226@qq.com>
2023-02-22 15:09:49 +0800

    feat:聊天框骨架构建

--- src/components/base/ChatBox.vue
+++ src/components/base/ChatBox.vue
@@ -1,17 +1,28 @@
 <template>
     <div class="chat" ref="chat_body" v-show="show_chat">
-        <el-container>
-            <el-header style="height: 2vh;">
-                <div class="float-end close" @click="close_chat">
-                    <i class="el-icon-close" style="margin:10px auto auto 10px; width: 100%; height: 100%;"></i>
+        <el-container style="height: 100%; width: 55vw;" >
+            <el-header height="15px">
+                <div class="pull-left">
+                    <i class="el-icon-chat-dot-round" style="margin:10px auto auto 10px;"></i>
+                    &nbsp;
+                    <span>聊天</span>
+                </div>
+                <div class="pull-right" @click="close_chat">
+                    <i class="el-icon-close" style="margin:10px 10px 10px 10px; "></i>
                 </div>
             </el-header>
             <el-divider></el-divider>
-            <el-container>
-                <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
-                    
+            <el-container style="margin-top:-25px; width: 55vw">
+                <el-aside width="225px" class="chat_aside">
+
                 </el-aside>
-                <el-main>Main</el-main>
+                <el-main class="chat_main">
+                    <el-divider style="width: 100vw; margin-left: -75px; margin-top: 50px;"></el-divider>
+                    <div class="chat_record">
+
+                    </div>
+                    <el-input type="textarea" :rows="6" v-model="chat_msg"></el-input>
+                </el-main>
             </el-container>
         </el-container>
     </div>
@@ -32,6 +43,7 @@ export default {
 
     setup(props, context) {
         let chat_body = ref(null);
+        let chat_msg = ref("");
         onMounted(() => {
             new ChatBody(chat_body.value);
         });
@@ -43,30 +55,65 @@ export default {
         return {
             chat_body,
             close_chat,
+            chat_msg,
         }
     }
 }
 </script>
 
 <style scoped>
+
+* {
+    padding: 0px 0px 0px 0px;
+}
 .chat {
     width: 55vw;
     height: 60vh;
     background-color: whitesmoke;
     position: absolute;
+    border: 0.1px solid;
+    border-radius: 5px;
     top: 20%;
     left: 20%;
     box-shadow: 2px 2px 2px rgba(215, 207, 207, 0.7);
     z-index: 999;
 }
 
-.close {
+.pull-left {
+    float: left !important;
+    margin: 0 auto;
+    height: 5vh;
+}
+
+.pull-right {
+    float: right !important;
     margin: 0 auto;
     height: 4vh;
     width: 4vh;
 }
 
-.close:hover {
-    background-color: rgba(98, 53, 53, 0.7);    
+.pull-right:hover {
+    background-color: rgba(98, 34, 34, 0.7);
 }
+
+.chat_aside {
+    height: 100%;
+    box-sizing: border-box;
+    background-color: rgba(166, 159, 159, 0.2);
+}
+
+.chat_main {
+    background-color: white;
+    padding: 0px 0px 0px 0px;
+}
+
+.chat_record {
+    height: 57%;
+    padding: 0px 0px 0px 0px;
+    margin-top: -25px;
+    background-color: rgb(151, 154, 153);
+}
+
+
+
 </style>
