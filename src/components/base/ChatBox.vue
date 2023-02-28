ed05db1c9fb692c6c96f3960487109848ed396bf
Oli51467 <1832297226@qq.com>
2023-02-28 10:53:56 +0800

    refactor:重构聊天界面

--- src/components/base/ChatBox.vue
+++ src/components/base/ChatBox.vue
@@ -1,38 +1,39 @@
 <template>
     <div class="chat" ref="chat_body" v-show="show_chat">
-        <el-container style="height: 477px; width: 833px;">
-            <el-header height="15px">
-                <div class="pull-left">
-                    <img src="@/assets/images/chat_logo.png" />
-                    &nbsp;
-                    <span>聊天</span>
+        <div class="header">
+            <div class="pull-left">
+                <img src="@/assets/images/chat_logo.png" />
+                &nbsp;
+                <span>聊天</span>
+            </div>
+            <span class="close" v-on:click="close_chat">✕</span>
+        </div>
+        <div class="container">
+            <div class="friend-list">
+                <h3>好友列表</h3>
+                <ul>
+                    <li v-for="friend in friends" :key="friend.id" v-on:click="selectFriend(friend)">
+                        {{ friend.name }}
+                    </li>
+                </ul>
+            </div>
+            <div class="chat-window">
+                <div class="title_header">
+                    <h3>{{ selectedFriend.name }}</h3>
                 </div>
-                <div class="pull-right" @click.prevent="close_chat">
-                    <img src="@/assets/images/close.png" />
+                <div class="messages">
+                    <div class="message" v-for="message in selectedFriend.messages" :key="message.id" :class="message.type">
+                        {{ message.content }}
+                    </div>
                 </div>
-            </el-header>
-            <el-divider></el-divider>
+                <div class="input">
+                    <el-input type="textarea" :rows="4" v-model="chat_msg" class="in" placeholder="Type a message...">
+                    </el-input>
+                    <el-button type="info" id="send" class="" plain>发送</el-button>
+                </div>
 
-            <el-container style="margin-top:-25px; width: 833px">
-                <el-aside width="225px" class="chat_aside">
-
-                </el-aside>
-                <el-main class="chat_main">
-                    <el-divider style="width: 680px; margin-left: -75px; margin-top: 50px;"></el-divider>
-                    <div class="chat_record">
-
-                    </div>
-
-                    <div class="d-flex align-items-end flex-column mb-3">
-                        <el-input type="textarea" :rows="4" v-model="chat_msg" :autofocus=true class="p-2">
-                        </el-input>
-                        <el-button type="info" id="send" class="p-2">发送</el-button>
-                    </div>
-
-
-                </el-main>
-            </el-container>
-        </el-container>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -62,8 +63,44 @@ export default {
 
         return {
             chat_body,
-            close_chat,
             chat_msg,
+            close_chat,
+        }
+    },
+
+    data() {
+        return {
+            friends: [
+                {
+                    id: 1, name: 'Alice', messages: [
+                        { id: 1, content: 'Hello!', type: 'sent' },
+                        { id: 2, content: 'Hi there!', type: 'received' },
+                        { id: 3, content: 'How are you?', type: 'sent' },
+                        { id: 4, content: 'I\'m doing well, thanks. How about you?', type: 'received' },
+                        { id: 5, content: 'I\'m good too.', type: 'sent' },
+                    ],
+                },
+                { id: 2, name: 'Bob', messages: ["hello"] },
+                { id: 3, name: 'Charlie', messages: ["2312"] },
+                { id: 4, name: 'David', messages: [] },
+            ],
+            selectedFriend: { id: null, name: null, messages: [] },
+            messageInput: '',
+        }
+    },
+    methods: {
+        selectFriend(friend) {
+            this.selectedFriend = friend;
+        },
+        sendMessage() {
+            if (this.messageInput.trim() !== '') {
+                this.selectedFriend.messages.push({ id: this.selectedFriend.messages.length + 1, content: this.messageInput, type: 'sent' });
+                this.messageInput = '';
+                // scroll to bottom of messages
+                this.$nextTick(() => {
+                    this.$refs.window.querySelector('.messages').scrollTop = this.$refs.window.querySelector('.messages').scrollHeight;
+                });
+            }
         }
     }
 }
@@ -77,7 +114,7 @@ export default {
 .chat {
     width: 833px;
     height: 477px;
-    background-color: whitesmoke;
+    background-color: white;
     position: absolute;
     border: 0.1px solid;
     border-radius: 5px;
@@ -93,42 +130,143 @@ export default {
     height: 50px;
 }
 
-.pull-right {
-    float: right;
-    margin: 5px 0px 0px auto;
-    height: 35px;
-    width: 35px;
-}
-
-.pull-right:hover {
-    background-color: rgba(98, 34, 34, 0.7);
-}
-
-.chat_aside {
-    height: 100%;
-    box-sizing: border-box;
-    background-color: rgba(166, 159, 159, 0.2);
-}
-
-.chat_main {
-    background-color: white;
-    padding: 0px 0px 0px 0px;
-    height: 100%;
-}
-
-.chat_record {
-    height: 230px;
-    padding: 0px 0px 0px 0px;
-    margin-top: -25px;
-    background-color: rgba(206, 201, 201, 0.2);
-}
-
 img {
     width: 30px;
     height: 30px;
 }
 
-#send {
-    margin: 0 10px auto;
+/* .messages {
+    overflow-y: scroll;
+    height: 205px;
+    padding: 10px;
+    margin-top: -25px;
+    background-color: rgba(206, 201, 201, 0.2);
+}
+
+.message {
+    margin-bottom: 10px;
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    background-color: #f7f7f7;
+}
+
+.message.sent {
+    text-align: right;
+}
+
+.message.received {
+    text-align: left;
+} */
+
+.header {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    height: 40px;
+    padding: 0 10px;
+    border-top: 1px solid #ccc;
+    border-left: 1px solid #ccc;
+    border-right: 1px solid #ccc;
+    background-color: #f7f7f7;
+    user-select: none;
+}
+
+.close {
+    font-size: 20px;
+    font-weight: bold;
+    color: #999;
+    cursor: pointer;
+}
+
+.close:hover {
+    background-color: rgba(98, 34, 34, 0.7);
+}
+
+.container {
+    display: flex;
+    height: 92%;
+}
+
+.friend-list {
+    width: 30%;
+    border-right: 1px solid #ccc;
+    background-color: rgba(166, 159, 159, 0.2);
+    text-align: center;
+}
+
+.friend-list ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+}
+
+.friend-list li {
+    padding: 10px 20px;
+    border-bottom: 1px solid #eee;
+    background-color: white;
+}
+
+.friend-list li:last-child {
+    border-bottom: none;
+}
+
+.friend-list li:hover {
+    background-color: #f7f7f7;
+}
+
+.chat-window {
+    width: 70%;
+    display: flex;
+    flex-direction: column;
+}
+
+.title_header {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 50px;
+    border-bottom: 1px solid #ccc;
+    background-color: #f7f7f7;
+}
+
+.messages {
+    flex: 1;
+    overflow-y: scroll;
+    padding: 10px;
+}
+
+.message {
+    padding: 5px;
+    margin-bottom: 5px;
+    border-radius: 5px;
+}
+
+.message.sent {
+    background-color: #f7f7f7;
+    text-align: right;
+}
+
+.message.received {
+    background-color: #fff;
+    text-align: left;
+}
+
+.input {
+    display: flex;
+    height: 97px;
+    border-top: 1px solid #ccc;
+}
+
+.input.el-input {
+    flex: 1;
+    border: none;
+    border-radius: 5px;
+}
+
+.input button {
+    padding: 10px;
+    border: none;
+    border-radius: 5px;
 }
 </style>
