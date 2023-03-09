f8e8033810724598a15c0de4920e22d42b0de6b8
oliieeee <>
2023-03-09 16:26:17 +0800

    test

--- src/assets/apis/api.js
+++ src/assets/apis/api.js
@@ -1,7 +1,7 @@
 import Axios from "axios";
 import { post } from 'jquery';
 
-// const API_URL = "https://web.fcjznkj.com/api";
+// const API_URL = "https://web.fcjznkj.com/api"; 
 // const WS_URL = "wss://web.fcjznkj.com/go/websocket";
 const WS_URL = "ws://127.0.0.1:3000/go/websocket";
 const API_URL = "http://127.0.0.1:3000/api";
